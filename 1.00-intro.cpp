47fcafbd3fe1c73a007e9a0d65645bb710a1485f
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-26 22:27:33 +0700

    v1 (1.00)

---
+++ 1.00-intro.cpp
@@ -0,0 +1,16 @@
+/* 
+Deskripsi : Menampilkan bilangan dua kali lipat
+Nama / Author : Ihsan Fauzi
+Tanggal / Versi : 21 Oktober 2024
+*/
+
+#include <iostream> // Header file untuk input-output di C++
+using namespace std;
+
+int main() {
+    int x, y, result;
+    cin >> x >> y; // Mengambil input dari pengguna
+    result = x + y; // Menghitung hasil
+    cout << result; // Menampilkan hasil
+    return 0;
+}
