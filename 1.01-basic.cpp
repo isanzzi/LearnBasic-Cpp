7111ba59bbf6bcd4c082163448f980b389a0ef51
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-27 21:05:44 +0700

    v2(1.01)

---
+++ 1.01-basic.cpp
@@ -0,0 +1,18 @@
+/* 
+Deskripsi : Menampilkan bilangan dua kali lipat
+Nama / Author : Ihsan Fauzi
+Tanggal / Versi : 21 Oktober 2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // tulis jawaban Anda di bawah ini
+    int x, result;
+    cin >> x; // Input nilai x
+    result = x * 2; // Menghitung dua kali lipat nilai x
+    cout << result; // Menampilkan hasil
+
+    return 0;
+}
