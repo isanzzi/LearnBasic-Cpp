8c283e7f1bbcb9bae512c4f1f212734bab22c770
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-28 23:30:03 +0700

    v3 (1.02)

---
+++ 1.02-basic.cpp
@@ -0,0 +1,21 @@
+/* 
+Deskripsi : Mengurutkan bilangan dari x sampai y, setiap bilangan dipisahkan oleh spasi
+Nama /Author : Ihsan Fauzi
+Tanggal/versi : 21 Oktober 2024
+*/
+
+#include <iostream> // untuk input-output stream
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y; // input dua bilangan
+    
+    while (x <= y) {
+        cout << x << " "; // output bilangan dipisahkan oleh spasi
+        x++;
+    }
+    
+    return 0;
+}
