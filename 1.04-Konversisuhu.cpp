47a0a7c006127b06c1550719ae2f5f32e9709e6a
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-30 23:41:38 +0700

    v5 (1.04)

---
+++ 1.04-Konversisuhu.cpp
@@ -0,0 +1,21 @@
+/* Program : CelciusToFahrenheit.cpp
+ * Deskripsi : Mengonversi suhu dalam celcius ke fahrenheit
+ * Nama : Ihsan Fauzi
+ * Tanggal/ Versi : 21 Oktober 2024
+ * Catatan: °F = °C × 1,8 + 32
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Kamus Data */
+    float fahrenheit, celcius;
+    
+    /* Algoritma */
+    cin >> celcius;
+    fahrenheit = celcius * 1.8 + 32;
+    cout << fahrenheit;
+    
+    return 0;
+}
