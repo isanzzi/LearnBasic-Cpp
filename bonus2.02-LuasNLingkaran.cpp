f28dd93635dac8485e3c3e78c5b66202daef62e1
SiKodot <ihsangamersfauzi123@gmail.com>
2025-02-02 22:24:24 +0700

    v8(bonus 2)

---
+++ bonus2.02-LuasNLingkaran.cpp
@@ -0,0 +1,19 @@
+/*
+Nama    : Ihsan Fauzi
+Kelas   : 1B-D4
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    float r, luas;
+    
+    cin >> r >> n;
+    luas = r * r * n * 3.14;
+    
+    cout << luas << endl;
+    
+    return 0;
+}
