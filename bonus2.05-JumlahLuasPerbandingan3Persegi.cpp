cd6caa4233cf922a35d5f0ece970c50631759c4f
SiKodot <ihsangamersfauzi123@gmail.com>
2025-02-06 22:43:17 +0700

    v12 (bonus 5)

---
+++ bonus2.05-JumlahLuasPerbandingan3Persegi.cpp
@@ -0,0 +1,35 @@
+/*
+Nama    : Ihsan Fauzi
+Kelas   : 1B-D4
+NIM     : 241524048
+*/
+
+#include <iostream>
+#include <iomanip> // Untuk std::setprecision
+
+int main() {
+    int x, y, z;
+    float zz, hasil1, hasil2, hasil3, totalhasil;
+
+    // Menggunakan cin untuk input
+    std::cin >> x >> y >> z >> zz;
+
+    // Menghitung hasil
+    hasil1 = zz * x / x; // Ini sama dengan zz
+    hasil2 = zz * y / x; 
+    hasil3 = zz * z / x; 
+
+    // Menghitung kuadrat dari hasil
+    hasil1 = hasil1 * hasil1;
+    hasil2 = hasil2 * hasil2;
+    hasil3 = hasil3 * hasil3;
+
+    // Menghitung total hasil
+    totalhasil = hasil1 + hasil2 + hasil3;
+
+    // Mengatur format output
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << totalhasil << std::endl;
+
+    return 0;
+}
