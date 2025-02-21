3ce40d49a6060accd2e7ddd64538ece4c3d18401
SiKodot <ihsangamersfauzi123@gmail.com>
2025-02-21 23:42:11 +0700

    v22 (2.16)

---
+++ 2.16-Memeriksa Huruf Vokal.cpp
@@ -0,0 +1,15 @@
+
+ #include<stdio.h>
+
+ int main(){
+     char x;
+     scanf ("%c", &x);
+         if (x=='a'||x=='i'||x=='u'||x=='e'||x=='o'||x=='A'||x=='I'||x=='U'||x=='E'||x=='O'){
+             printf ("true");
+         }
+         else{
+             printf ("false");
+             
+         }
+     }
+   
