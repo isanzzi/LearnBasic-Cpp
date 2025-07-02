36ab76cf82c10d60048fe5811681b332de555003
SiKodot <ihsangamersfauzi123@gmail.com>
2025-07-02 23:42:27 +0700

    v41 (4.60)

---
+++ 4.60-FPBDuaVariable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    std::cout << a;
+    return 0;
+}
